dbdaac2f02da6069d36439c37b2993f3cdb282d6
Oliver Cristian <shobolanica@gmail.com>
2019-01-19 13:56:29 +0200

    Added error handling.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="electron"/>
 export function disable(window: Electron.BrowserWindow, color: number): boolean;
 export function setGradient(window: Electron.BrowserWindow, color: number): boolean;
 export function setTransparentGradient(window: Electron.BrowserWindow, color: number): boolean;
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-"use strict";
+`use strict`;
 
-const ewc = require('bindings')('ewc');
+const ewc = require(`bindings`)(`ewc`);
+
+const SWCA = (window, accent, tint) => {
+	window = window.getNativeWindowHandle();
+	if (!window instanceof Buffer) {
+		throw new Error(`The 'window' argument is not a native window handler.`);
+	}
+	if(typeof tint != `number`) {
+		throw new Errow(`The 'tint' argument is not a number.`);
+	}
+	return ewc.setComposition(window, accent, tint);
+}
 
 module.exports = {
 	disable: (window) => {
-		return ewc.setComposition(window.getNativeWindowHandle(), 0, 0xFFFFFFFF);
+		window = window.getNativeWindowHandle();
+		if (window instanceof Buffer) {
+			return ewc.setComposition(window, 0, 0x00000000);
+		} else {
+			throw new Error(`The 'window' argument is not a native window handler.`)
+		}
 	},
 	setGradient: (window, tint) => {
-		return ewc.setComposition(window.getNativeWindowHandle(), 1, tint);
+		return SWCA(window, 1, tint);
 	},
 	setTransparentGradient: (window, tint) => {
-		return ewc.setComposition(window.getNativeWindowHandle(), 2, tint);
+		return SWCA(window, 2, tint);
 	},
 	setBlurBehind: (window, tint) => {
-		return ewc.setComposition(window.getNativeWindowHandle(), 3, tint);
+		return SWCA(window, 3, tint);
 	},
     setAcrylic: (window, tint) => {
-        return ewc.setComposition(window.getNativeWindowHandle(), 4, tint);   
+		return SWCA(window, 4, tint);
     }
 }
